27129930215bd12770d5c192fceceec222041686
griesfeller <webmaster@griesfeller.de>
2018-04-03 18:37:24 +0000

    new form mit fehler

--- src/Controller/GnetCustomerController.php
+++ src/Controller/GnetCustomerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use App\Entity\GnetCustomer;
@@ -21,5 +22,36 @@ class GnetCustomerController extends Controller
             'controller_name' => 'GnetCustomerController',
             'gnetCustomerId' => $gnetCustomer->getId(),
         ]);
+    }
+    
+    
+    /**
+     * @Route("/gnet/customer/new", name="gnet_customer_new")
+     */
+    public function new(Request $request)
+    {
+        $gnetCustomer = new GnetCustomer();
+        $form = $this->createFormBuilder($gnetCustomer)
+            ->add('salutation')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('middleName')
+            ->add('street')
+            ->add('housenumber')
+            ->add('addressAddtitionals')
+            ->add('city')
+            ->add('postalCode')
+            ->add('telephon')
+            ->add('fax')
+            ->add('mobil')
+            ->add('email')->getForm();
+            
+            if ($form->isSubmitted() && $form->isValid()) {
+                return $this->redirectToRoute('task_success');
+            }
+            
+        return $this->render('gnet_customer/new.html.twig', array(
+        'form' => $form->createView(),
+        ));
     }
 }
